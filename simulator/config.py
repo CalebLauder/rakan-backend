0d5cf6734dd581acd9b23cdacf4b6ea30bc86746
Caleb <caleblauder@yahoo.com>
2025-11-20 21:49:56 -0500

    chore: remove duplicate simulators folder

--- simulator/config.py
+++
@@ -1,8 +0,0 @@
-MQTT_ENDPOINT = "localhost"   # or AWS endpoint
-CA_PATH = "certs/AmazonRootCA1.pem"
-CERT_PATH = "certs/device.pem.crt"
-KEY_PATH = "certs/private.pem.key"
-EVENTS_TOPIC = "rakan/events"
-COMMANDS_TOPIC_FMT = "rakan/commands/{deviceId}"
-QOS = 1
-USE_TLS = true

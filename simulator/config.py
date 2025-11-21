362a4bc1c06c72e29eacd6cadf1660e2544d5de8
Robert N Reed <120078763+RobertReed1412@users.noreply.github.com>
2025-11-20 22:16:00 -0500

    Update MQTT configuration for IoT connection

--- simulator/config.py
+++ simulator/config.py
@@ -1,8 +1,9 @@
-MQTT_ENDPOINT = "localhost"   # or AWS endpoint
-CA_PATH = "certs/AmazonRootCA1.pem"
-CERT_PATH = "certs/device.pem.crt"
-KEY_PATH = "certs/private.pem.key"
-EVENTS_TOPIC = "rakan/events"
-COMMANDS_TOPIC_FMT = "rakan/commands/{deviceId}"
-QOS = 1
-USE_TLS = true
+IOT_HOST = "a2cu3qgpy7qzdx-ats.iot.us-east-1.amazonaws.com"
+
+CA_PATH = "simulator/certs/AmazonRootCA1.pem"
+CERT_PATH = "simulator/certs/dev-device.pem.crt"
+KEY_PATH = "simulator/certs/dev-private.pem.key"
+
+EVENT_TOPIC = "rakan/events"
+COMMAND_TOPIC = "rakan/commands/{deviceId}"
+MQTT_PORT = 8883

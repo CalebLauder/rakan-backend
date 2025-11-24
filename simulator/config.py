2b67d91b7b2059b3534f4fea0ba9d8847e5a4f6d
Robert N Reed <120078763+RobertReed1412@users.noreply.github.com>
2025-11-24 18:22:43 -0500

    Refactor MQTT configuration parameters

--- simulator/config.py
+++ simulator/config.py
@@ -1,9 +1,7 @@
-IOT_HOST = "a2cu3qgpy7qzdx-ats.iot.us-east-1.amazonaws.com"
+MQTT_ENDPOINT = "a2cu3qgpy7qzdx-ats.iot.us-east-1.amazonaws.com"
+CA_PATH = "certs/AmazonRootCA1.pem"
+CERT_PATH = "certs/device.pem.crt"
+KEY_PATH = "certs/private.pem.key"
+EVENTS_TOPIC = "rakan/events"
+topic = "rakan/commands/" + deviceId
 
-CA_PATH = "simulator/certs/AmazonRootCA1.pem"
-CERT_PATH = "simulator/certs/dev-device.pem.crt"
-KEY_PATH = "simulator/certs/dev-private.pem.key"
-
-EVENT_TOPIC = "rakan/events"
-COMMAND_TOPIC = "rakan/commands/{deviceId}"
-MQTT_PORT = 8883

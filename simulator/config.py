87610b7a929304b0312c04fddaba61f1777f17b9
Robert N Reed <120078763+RobertReed1412@users.noreply.github.com>
2025-11-24 18:37:02 -0500

    Update MQTT configuration parameters

--- simulator/config.py
+++ simulator/config.py
@@ -1,7 +1,11 @@
 MQTT_ENDPOINT = "a2cu3qgpy7qzdx-ats.iot.us-east-1.amazonaws.com"
+
 CA_PATH = "certs/AmazonRootCA1.pem"
 CERT_PATH = "certs/device.pem.crt"
 KEY_PATH = "certs/private.pem.key"
-EVENTS_TOPIC = "rakan/events"
-topic = "rakan/commands/" + deviceId
 
+EVENTS_TOPIC = "rakan/events"
+COMMANDS_TOPIC_FMT = "rakan/commands/{deviceId}"
+
+QOS = 1
+USE_TLS = True

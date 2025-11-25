0cc947f41df777fbc35611bd780b6a05ba44426d
Robert N Reed <120078763+RobertReed1412@users.noreply.github.com>
2025-11-24 19:46:18 -0500

    Update MQTT config to use dynamic path resolution
    Refactor certificate paths to use BASE_DIR for better portability.

--- simulator/config.py
+++ simulator/config.py
@@ -1,9 +1,12 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
 MQTT_ENDPOINT = "a2cu3qgpy7qzdx-ats.iot.us-east-1.amazonaws.com"
 
-CA_PATH = "../certs/AmazonRootCA1.pem"
-CERT_PATH = "../certs/device.pem.crt"
-KEY_PATH = "../certs/private.pem.key"
-
+CA_PATH   = os.path.join(BASE_DIR, "certs", "AmazonRootCA1.pem")
+CERT_PATH = os.path.join(BASE_DIR, "certs", "dev-device.pem.crt")
+KEY_PATH  = os.path.join(BASE_DIR, "certs", "dev-private.pem.key")
 
 EVENTS_TOPIC = "rakan/events"
 COMMANDS_TOPIC_FMT = "rakan/commands/{deviceId}"
